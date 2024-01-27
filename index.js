960b7443993c40d31e5956154cfd99a4f53f21c9
codewithzaman <naderzaman50@gmail.com>
2024-01-28 00:11:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //  NPM is a package manager for Node.js packages, or modules
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World! home page')
-})
-app.get('/about', (req, res) => {
-    res.send('Hello World! about page')
+// app.get('/', (req, res) => {
+//   res.send('Hello World! home page')
+// })
+// app.get('/about', (req, res) => {
+//     res.send('Hello World! about page')
+//   })
+  app.get ('/' , (req,res)=>{
+    res.sendFile(path.join(__dirname , 'index.html'))
   })
-  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
