b60af0071a46a2ef66594e0a77695da8546e7d3c
codewithzaman <naderzaman50@gmail.com>
2024-01-27 23:46:54 +0500

    Update index.js
    Created server with express js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //  NPM is a package manager for Node.js packages, or modules
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! home page')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
